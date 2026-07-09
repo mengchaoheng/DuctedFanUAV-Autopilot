d78608e83dc2d0b4d8eab0bf4d5070592de6f843
mengchaoheng <chaohengmeng@163.com>
2026-07-09 13:59:01 +0800

    reset mc att

--- src/modules/mc_att_control/AttitudeControl/AttitudeControl.cpp
+++ src/modules/mc_att_control/AttitudeControl/AttitudeControl.cpp
@@ -61,35 +61,6 @@ void AttitudeControl::setProportionalGain(const matrix::Vector3f &proportional_g
 matrix::Vector3f AttitudeControl::update(const Quatf &q) const
 {
 	Quatf qd = _attitude_setpoint_q;
-	// Ezra Tal
-	// const float yaw_ref = Eulerf(qd).psi();
-	// Tal & Karaman (2021) Eq. (22)-(26): compute xi_c = \bar{xi}_c \circ xi_psi
-	// const Vector3f i_z{0.f, 0.f, 1.f};
-	// const Vector3f minus_bz_c = -qd.dcm_z();
-	// const Vector3f minus_bz_c_b = Dcmf(q.inversed()) * minus_bz_c;
-
-	// const float dot_iz = i_z.dot(minus_bz_c_b);
-	// const Vector3f cross_iz = i_z.cross(minus_bz_c_b);
-
-	// Quatf bar_xi_c{1.f - dot_iz, -cross_iz(0), -cross_iz(1), -cross_iz(2)};
-	// bar_xi_c.normalize();
-
-	// const Vector3f n_psi_ref{sinf(yaw_ref), -cosf(yaw_ref), 0.f};
-	// const Quatf q_tilt = q * bar_xi_c; // qd_red
-	// const Vector3f bar_n_psi_ref = Dcmf(q_tilt.inversed()) * n_psi_ref;
-
-	// const float n1 = bar_n_psi_ref(0);
-	// const float n2 = bar_n_psi_ref(1);
-	// const float kappa = (fabsf(n2) > 1e-6f) ? (-n1 / n2) : 0.f;
-
-	// Quatf xi_psi{1.f, 0.f, 0.f, kappa/(1+sqrtf(1.f + kappa * kappa))};
-	// xi_psi.normalize();
-
-	// const Quatf xi_c = bar_xi_c * xi_psi; // = qe
-
-	// PX4_INFO("xi_c: [%.3f %.3f %.3f %.3f]", (double)xi_c(0), (double)xi_c(1), (double)xi_c(2), (double)xi_c(3));
-
-
 
 	// calculate reduced desired attitude neglecting vehicle's yaw to prioritize roll and pitch
 	const Vector3f e_z = q.dcm_z();
@@ -106,38 +77,14 @@ matrix::Vector3f AttitudeControl::update(const Quatf &q) const
 	} else {
 		// Transform rotation from current to desired thrust vector into a world frame reduced desired attitude.
 		// This is a right multiplication as the tilt error quaternion is obtained from two Z vectors expressed in the world frame.
-		Quatf q_e_red = q.inversed() * q_e_red_I * q; // bar_xi_c ?
+		Quatf q_e_red = q.inversed() * q_e_red_I * q; // bar_xi_c
 		qd_red =q * q_e_red; // xi * bar_xi_c = q_tilt = q * bar_xi_c
 	}
 
 	// With a full desired attitude given by: qd = qd_red * qd_dyaw, extract the delta yaw component.
 	// By definition, the delta yaw quaternion has the form (cos(angle/2), 0, 0, sin(angle/2))
-	// const Quatf q_e_red_cmp = q.inversed() * qd_red;
-
-	// PX4_INFO("q_e_red-bar_xi_c: [%.6f %.6f %.6f %.6f]",
-	// 	 (double)(q_e_red_cmp(0) - bar_xi_c(0)),
-	// 	 (double)(q_e_red_cmp(1) - bar_xi_c(1)),
-	// 	 (double)(q_e_red_cmp(2) - bar_xi_c(2)),
-	// 	 (double)(q_e_red_cmp(3) - bar_xi_c(3)));
-	// PX4_INFO("qd_red-q_tilt: [%.6f %.6f %.6f %.6f]",
-	// 	 (double)(qd_red(0) - q_tilt(0)),
-	// 	 (double)(qd_red(1) - q_tilt(1)),
-	// 	 (double)(qd_red(2) - q_tilt(2)),
-	// 	 (double)(qd_red(3) - q_tilt(3)));
-
 	Quatf qd_dyaw = qd_red.inversed() * qd;
 	qd_dyaw.canonicalize();
-
-	// const Quatf q_xi_psi_delta = xi_psi.inversed() * qd_dyaw;
-	// const float angle_diff_xi_psi_qd_dyaw = 2.f * atan2f(q_xi_psi_delta.imag().norm(), fabsf(q_xi_psi_delta(0)));
-	// PX4_INFO("xi_psi-qd_dyaw: [%.6f %.6f %.6f %.6f], angle_diff: %.6f rad",
-	// 	 (double)(xi_psi(0) - qd_dyaw(0)),
-	// 	 (double)(xi_psi(1) - qd_dyaw(1)),
-	// 	 (double)(xi_psi(2) - qd_dyaw(2)),
-	// 	 (double)(xi_psi(3) - qd_dyaw(3)),
-	// 	 (double)angle_diff_xi_psi_qd_dyaw);
-
-
 	// catch numerical problems with the domain of acosf and asinf
 	qd_dyaw(0) = math::constrain(qd_dyaw(0), -1.f, 1.f);
 	qd_dyaw(3) = math::constrain(qd_dyaw(3), -1.f, 1.f);
@@ -148,11 +95,9 @@ matrix::Vector3f AttitudeControl::update(const Quatf &q) const
 	// quaternion attitude control law, qe is rotation from q to qd
 	const Quatf qe = q.inversed() * qd;
 
-	// qe = q_e_red * Quatf(cosf(_yaw_w * acosf(qd_dyaw(0))), 0.f, 0.f, sinf(_yaw_w * asinf(qd_dyaw(3)))) = xi_c;
-
 	// using sin(alpha/2) scaled rotation axis as attitude error (see quaternion definition by axis angle)
 	// also taking care of the antipodal unit quaternion ambiguity
-	const Vector3f eq = 2.f * qe.canonical().imag(); //由于q和-q都是表示同一个姿态。canonical是规范化，使得实部为正, 同时还考虑了实部很小时认为是0（参考文献中sgn(qe_0)=1），然后将寻找第一个不为零的数，据此调整符号（使其为正）, 确保四元数虚部唯一。这时候直接使用虚部即可作姿态误差对应的参考角速度。
+	const Vector3f eq = 2.f * qe.canonical().imag(); // Since q and -q represent the same orientation. Canonical is a normalization process that ensures the real part is positive, and also considers the real part as zero when it is very small (as referenced by sgn(qe_0)=1). Then, the first non-zero number is found to adjust the sign (making it positive), ensuring the uniqueness of the quaternion's imaginary part. At this point, the imaginary part can be directly used as the reference angular velocity corresponding to the orientation error.
 
 	// calculate angular rates setpoint
 	Vector3f rate_setpoint = eq.emult(_proportional_gain);
